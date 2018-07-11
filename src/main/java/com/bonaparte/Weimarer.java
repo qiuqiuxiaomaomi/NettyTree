5fa40692787c55cb6e3e5eead35691107d30255b
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-11 18:27:35 +0800

    Netty工程初始化

--- src/main/java/com/bonaparte/Weimarer.java
+++ src/main/java/com/bonaparte/Weimarer.java
@@ -1,7 +1,17 @@
 package com.bonaparte;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /**
  * Created by yangmingquan on 2018/7/11.
  */
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableAsync
+@EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 public class Weimarer {
 }
